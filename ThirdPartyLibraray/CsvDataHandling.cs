b766c78cd02c724dcbd29e9a543646aaa6479048
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-12-12 13:27:11 +0530

    Added File

---
+++ ThirdPartyLibraray/CsvDataHandling.cs
@@ -0,0 +1,46 @@
+﻿using CsvHelper;
+using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdPartyLibraray
+{
+    public class CsvDataHandling
+    {
+        private TextReader reader;
+        private TextWriter writer;
+
+        public void ImplementCvsDataHandling()
+        {
+            string importfilepath = @"E:\Basic Core Program\ThirdPartyLibraray\ThirdPartyLibraray\Address.csv";
+            string exportfilepath = @"E:\Basic Core Program\ThirdPartyLibraray\ThirdPartyLibraray\Export.csv";
+
+            using (var reader = new StreamReader(importfilepath)) ;
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read Data Successfully From Address.csv File");
+                foreach (AddressData addressData in records)
+                {
+                    Console.Write("\t" + addressData.Firstname);
+                    Console.Write("\t" + addressData.Lastname);
+                    Console.Write("\t" + addressData.Address);
+                    Console.Write("\t" + addressData.City);
+                    Console.Write("\t" + addressData.State);
+                    Console.Write("\t" + addressData.ZipCode);
+                    Console.Write("\n");
+                }
+                Console.WriteLine("Now Reading From Csv File Write to Csv ");
+                using (var writer = new StreamWriter(exportfilepath)) ;
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+            }
+        }
+    }
+}
---
+++ ThirdPartyLibraray/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ThirdPartyLibraray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Read Data From CSV and Write Data in CSV ");
+            CsvDataHandling csvDataHandling = new CsvDataHandling();
+            csvDataHandling.ImplementCvsDataHandling();
+        }
+    }
+}
